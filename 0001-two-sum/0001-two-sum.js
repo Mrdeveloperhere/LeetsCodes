36bb6a6ab45ad9e4ee39f09976d743ae7cfe19bd
Robin <161943524+Mrdeveloperhere@users.noreply.github.com>
2026-06-22 16:10:53 +0530

    Time: 2 ms (67.77%), Space: 54.6 MB (60.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,11 +1,17 @@
-var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] === target - nums[i]) {
-                return [i, j];
-            }
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map=new Map();
+    for(let i=0;i<nums.length;i++){
+        let diff = target - nums[i];
+
+        if(map.has(diff)){
+            return [map.get(diff),i]
         }
+
+        map.set(nums[i],i)
     }
-    // Return an empty array if no solution is found
-    return [];
-};
+}
